ff7fcf67925ceb6c8a171eace8a2625b6c3dc9d4
xyh <xue_yuanhuang@163.com>
2021-04-18 17:00:40 +0800

    test

--- src/cn/edu/jlu/domain/Person.java
+++ src/cn/edu/jlu/domain/Person.java
@@ -3,8 +3,5 @@ package cn.edu.jlu.domain;
 public class Person {
     private String id;
     private String name;
-
-    public Person(String id) {
-        this.id = id;
-    }
+    private String address;
 }
